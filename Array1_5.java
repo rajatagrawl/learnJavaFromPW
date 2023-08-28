564ee01dbf9234b88a041ccdaccea2c8bcbb48b9
Rajat Agrawal <rajat1611@gmail.com>
2023-08-28 07:43:23 +0530

    10-Aug-12:00PM

--- Array1_5.java
+++ Array1_5.java
@@ -1,6 +1,7 @@
 public class Array1_5 {
     public static void main(String[] args) {
         int[] arr = {1,1,3,4,2,35,7,0};
+        System.out.println("Hello World");
         for (int i=1; i<arr.length-1; i++){
             if(arr[i-1] < arr[i] && arr[i] > arr[i+1]){
                 System.out.println(arr[i]);
