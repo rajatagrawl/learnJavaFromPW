611e37c413695f5c2721b196958953fe1f8e725d
rajatagrawl <77034804+rajatagrawl@users.noreply.github.com>
2023-12-02 10:30:49 +0530

    Update Main.java

--- ArrayList/src/Main.java
+++ ArrayList/src/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("Hello world again !");
+//        System.out.println("Hello world  !");
 //        Integer in = new Integer(4);
         Integer in = Integer.valueOf(5);
         System.out.println(in);
